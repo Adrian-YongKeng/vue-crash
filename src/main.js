0593d922bed65d5081558d0f28889b7f055d4833
Adrian <yy_1123@hotmail.com>
2025-06-05 18:47:16 +0800

    implement router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+import router from "./router";
 import "./assets/main.css";
 import "primeicons/primeicons.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
