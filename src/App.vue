b976f1dc0fd32d4b0df10c94cc9d4a31c8be9077
Adrian <yy_1123@hotmail.com>
2025-06-05 18:04:29 +0800

    Install Tailwind css Use Computed function Pass Props Create json data

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,14 @@
 <template>
-  <h1>{{ name }}</h1>
-
-  <p v-if="status === 'active'">USer is active</p>
-  <p v-else-if="status === 'pending'">User is pending</p>
-  <p v-else>User is inactive</p>
-
-  <form @submit.prevent="addTask">
-    <label for="newTask">Add Task</label>
-    <input type="text" id="newTask" name="newTask" v-model="newTask" />
-
-    <button type="submit">Submit</button>
-  </form>
-
-  <h3>Tasks:</h3>
-  <ul>
-    <li v-for="(task, index) in tasks" :key="task">
-      <span>{{ task }}</span>
-
-      <button @click="deleteTask(index)">x</button>
-    </li>
-  </ul>
-
-  <br />
-  <button @click="toggleStatus">Change Status</button>
+  <Navbar />
+  <Hero />
+  <HomeCards />
+  <JobListings limit="3" :showButton="true" />
 </template>
 
 <script setup>
-// composition api
-import { onMounted, ref } from "vue";
-
-// export default {
-//   setup() {
-const name = ref("John Doe");
-const status = ref("active");
-const tasks = ref(["task one", "task two", "task three"]);
-const newTask = ref("");
-
-const toggleStatus = () => {
-  if (status.value === "active") {
-    status.value = "pending";
-  } else if (status.value === "pending") {
-    status.value = "inactive";
-  } else {
-    status.value = "active";
-  }
-};
-
-const addTask = () => {
-  if (newTask.value.trim() !== "") {
-    tasks.value.push(newTask.value);
-    newTask.value = "";
-  }
-};
-
-const deleteTask = (index) => {
-  tasks.value.splice(index, 1);
-};
-
-onMounted(async () => {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-    const data = await response.json();
-    tasks.value = data.map((task) => task.title);
-  } catch (error) {
-    console.log("Error fetching tasks");
-  }
-});
-
-//     return {
-//       name,
-//       status,
-//       tasks,
-//       toggleStatus,
-//     };
-//   },
-// };
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
 </script>
 
